2dfc78150210004a80f7ab4040665aaf504742d1
Fede_T <federicotroncoso144@hotmail.com>
2022-11-13 22:00:39 -0300

    .

--- batalla_naval_poo/mainwindow.cpp
+++ batalla_naval_poo/mainwindow.cpp
@@ -394,7 +394,69 @@ void MainWindow::on_btnColocar_clicked()
         }
 
     }else{
+        if(this->jug == 1){
+            auxqstring = this->ui->coordX->text();
+            posx = auxqstring.toInt();
 
+            auxqstring = this->ui->coordY->text();
+            posy = auxqstring.toInt();
+
+            auxqstring = this->ui->btnOrientacion->currentText();
+            auxstring = auxqstring.toStdString();
+            orientacion = auxstring[0];
+            bool a = true;
+            do{
+                if(this->juego.cantBarcos[this->indexTipo] > 0){
+                    a = false;
+                    if(this->juego.verificarPosicion(this->indexTipo, posx, posy, orientacion, this->jug)){
+                        this->juego.colocarBarco(this->indexTipo, posx, posy, orientacion, this->jug);
+                        this->juego.cantBarcos[this->indexTipo]--;
+                        this->ui->coordX->clear();
+                        this->ui->coordY->clear();
+                        this->setGuia(this->jug, 'O');
+                        this->actualizarSprites('O');
+                        this->cantBarcosColocados++;
+                        for(int i = 0; i < 5; i++){
+                            if(this->juego.cantBarcos[i] != 0){
+                                switch (i){
+                                case 0:
+                                    this->ui->instruccionesLabel->setText("¡Soldado! Ingrese la posicion de un portaaviones.");
+                                    break;
+                                case 1:
+                                    this->ui->instruccionesLabel->setText("¡Soldado! Ingrese la posicion de un destructor.");
+                                    break;
+                                case 2:
+                                    this->ui->instruccionesLabel->setText("¡Soldado! Ingrese la posicion de un submarino.");
+                                    break;
+                                case 3:
+                                    this->ui->instruccionesLabel->setText("¡Soldado! Ingrese la posicion de un crucero.");
+                                    break;
+                                case 4:
+                                    this->ui->instruccionesLabel->setText("¡Soldado! Ingrese la posicion de un lancha.");
+                                    break;
+                                }
+                                break;
+                            }
+                        }
+                    }else{
+                        this->ErrorColocacion.exec();
+                        this->ui->coordX->clear();
+                        this->ui->coordY->clear();
+
+                    }
+                }else{
+                    this->indexTipo++;
+                }
+            }while(a == true);
+
+            if(cantBarcosColocados == this->juego.cantBarcosTotal){
+                this->ui->instruccionesLabel->setText("Bien hecho soldado. Todas las unidades colocadas.\nPresione \"terminar turno\" ¡Es una orden!");
+                this->ui->btnColocar->setEnabled(false);
+                this->ui->btnOrientacion->setEnabled(false);
+                this->ui->btnRandom->setEnabled(false);
+                this->ui->btnTerminarTurno->setEnabled(true);
+            }
+        }
     }
 }
 
@@ -450,11 +512,24 @@ void MainWindow::on_btnTerminarTurno_clicked()
             this->ui->btnTerminarTurno->setEnabled(false);
         }
     }else{
-        this->taparMapas();
-        this->ui->btnTerminarTurno->setEnabled(false);
-        this->cambioTurno.exec();
-        this->cambiarJugador();
-        this->ui->turno->setText(QString::number(this->jug));
+        if(this->modoColocacion == true){
+            if(this->jug == 1){
+                this->cambiarJugador();
+                this->taparMapas();
+                this->cambioTurno.exec();
+                this->ui->turno->setText(QString::number(this->jug));
+                this->ui->btnColocar->setEnabled(true);
+                this->ui->btnOrientacion->setEnabled(true);
+                this->ui->btnRandom->setEnabled(true);
+                this->ui->btnTerminarTurno->setEnabled(false);
+                this->setGuia(this->jug, 'R');
+                this->setGuia(this->jug, 'O');
+                this->actualizarSprites('R');
+                this->actualizarSprites('O');
+
+
+            }
+        }
     }
 }
 
